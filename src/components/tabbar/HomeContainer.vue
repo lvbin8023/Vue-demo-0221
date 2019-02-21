d366b3918ea23bbccafef02a02e60a601e5c80fc
lvbin <472245767@qq.com>
2019-02-21 23:12:10 +0800

    使用axios获取轮播图数据后加载

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -2,31 +2,53 @@
   <div>
     <!-- 轮播图区域 -->
     <mt-swipe :auto="4000">
-      <mt-swipe-item>1</mt-swipe-item>
-      <mt-swipe-item>2</mt-swipe-item>
-      <mt-swipe-item>3</mt-swipe-item>
+      <mt-swipe-item v-for="item in lunboList" :key="item.index">
+        <img :src="item.img" alt="">
+      </mt-swipe-item>
     </mt-swipe>
     <h1>这是home组件</h1>
   </div>
 </template>
 
 <script>
-export default {};
+import { Toast } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      lunboList: []
+    };
+  },
+  created() {
+    this.getLunbotu();
+  },
+  methods: {
+    getLunbotu() {
+      this.axios
+        .get("http://www.liulongbin.top:3005/api/getlunbo")
+        .then(result => {
+          // console.log(result.data);
+          if (result.data.status === 0) {
+            this.lunboList = result.data.message;
+            Toast("加载轮播图成功");
+          } else {
+            Toast("加载轮播图失败");
+          }
+        });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .mint-swipe {
   height: 200px;
   .mint-swipe-item {
-    &:nth-child(1) {
-      background-color: orange;
-    }
-    &:nth-child(2) {
-      background-color: green;
-    }
-    &:nth-child(3) {
-      background-color: red;
-    }
+    background-color: orange;
+  }
+  img {
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
