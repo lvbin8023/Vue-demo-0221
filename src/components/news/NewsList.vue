9a70de9503c39450a1feb554e831cb75c65f5bec
lvbin <472245767@qq.com>
2019-02-23 20:47:15 +0800

    优化渲染新闻列表页面代码

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -13,66 +13,6 @@
           </div>
         </a>
       </li>
-      <!-- <li class="mui-table-view-cell mui-media">
-        <a href="javascript:;">
-          <img class="mui-media-object mui-pull-left" src="../../img/cat.jpg">
-          <div class="mui-media-body">
-            <h3>木屋</h3>
-            <p class="mui-ellipsis">
-              <span>发表时间：2010-10-10 10:10:10</span>
-              <span>点击：278次</span>
-            </p>
-          </div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media">
-        <a href="javascript:;">
-          <img class="mui-media-object mui-pull-left" src="../../img/muwu.jpg">
-          <div class="mui-media-body">
-            <h3>CBD</h3>
-            <p class="mui-ellipsis">
-              <span>发表时间：2012-12-12 12:12:12</span>
-              <span>点击：286次</span>
-            </p>
-          </div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media">
-        <a href="javascript:;">
-          <img class="mui-media-object mui-pull-left" src="../../img/cbd.jpg">
-          <div class="mui-media-body">
-            <h3>幸福</h3>
-            <p class="mui-ellipsis">
-              <span>发表时间：2008-08-08 08:08:08</span>
-              <span>点击：266次</span>
-            </p>
-          </div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media">
-        <a href="javascript:;">
-          <img class="mui-media-object mui-pull-left" src="../../img/cat.jpg">
-          <div class="mui-media-body">
-            <h3>木屋</h3>
-            <p class="mui-ellipsis">
-              <span>发表时间：2010-10-10 10:10:10</span>
-              <span>点击：278次</span>
-            </p>
-          </div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media">
-        <a href="javascript:;">
-          <img class="mui-media-object mui-pull-left" src="../../img/muwu.jpg">
-          <div class="mui-media-body">
-            <h3>CBD</h3>
-            <p class="mui-ellipsis">
-              <span>发表时间：2012-12-12 12:12:12</span>
-              <span>点击：286次</span>
-            </p>
-          </div>
-        </a>
-      </li>-->
     </ul>
   </div>
 </template>
