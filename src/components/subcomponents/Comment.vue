aeffea867954e85248df60c66770fe9d96074120
lvbin <472245767@qq.com>
2019-02-24 11:23:24 +0800

    渲染评论页面的数据

--- src/components/subcomponents/Comment.vue
+++ src/components/subcomponents/Comment.vue
@@ -4,10 +4,10 @@
     <hr>
     <textarea placeholder="请输入要评论的内容（字数不超过120字）" maxlength="120"></textarea>
     <mt-button type="primary" size="large">发表评论</mt-button>
-    <div class="comment-list" v-for="item in comments" :key="item.id">
+    <div class="comment-list" v-for="(item,i) in comments" :key="item.id">
       <div class="comment-item">
-        <h3 class="comment-title">第1楼&nbsp;&nbsp;用户：匿名用户&nbsp;&nbsp;发表时间：2012-12-12 12:12:12</h3>
-        <p class="comment-body">锄禾日当午，汗滴禾下土</p>
+        <h3 class="comment-title">第{{i+1}}楼&nbsp;&nbsp;用户：{{item.user_name}}&nbsp;&nbsp;发表时间：{{item.add_time}}</h3>
+        <p class="comment-body">{{item.content}}</p>
       </div>
     </div>
     <mt-button type="danger" size="large">加载更多</mt-button>
@@ -33,7 +33,7 @@ export default {
         .get("https://lvbin8023.github.io/Vue-demo-0221/dist/Comment.json")
         .then(result => {
           if (result.data.status === 0) {
-            this.comments = result.data.message[this.id - 1];
+            this.comments = result.data.message;
           } else {
             Toast("获取评论失败");
           }
@@ -64,7 +64,7 @@ export default {
       }
       .comment-body {
         line-height: 35px;
-        text-indent: 2em;
+        text-indent: 1em;
       }
     }
   }
