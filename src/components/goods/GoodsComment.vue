78386b331b4061770bd1f53f274e155add6f215e
lvbin <472245767@qq.com>
2019-03-21 20:51:56 +0800

    添加商品详情的加入购物车动画

--- src/components/goods/GoodsComment.vue
+++ src/components/goods/GoodsComment.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h3>商品评论页面</h3>
+    <comment></comment>
   </div>
 </template>
 
 <script>
-export default {};
+import comment from '../subcomponents/Comment.vue'; 
+
+export default {
+  components:{
+    comment
+  }
+};
 </script>
 
 <style lang="scss" scoped>
--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="goodsinfo-container">
+    <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+      <div class="ball" v-show="ballFlag" ref="ball"></div>
+    </transition>
     <!-- 商品轮播区域 -->
     <div class="mui-card">
       <div class="mui-card-content">
@@ -31,7 +34,7 @@
           </p>
           <p>
             <mt-button type="primary" size="small">立即购买</mt-button>
-            <mt-button type="danger" size="small">加入购物车</mt-button>
+            <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
           </p>
         </div>
       </div>
@@ -64,7 +67,8 @@ export default {
   data() {
     return {
       id: this.$route.params.id,
-      goodsinfo: []
+      goodsinfo: [],
+      ballFlag: false
     };
   },
   created() {
@@ -87,6 +91,29 @@ export default {
     },
     goComment(id) {
       this.$router.push({ name: "goodscomment", params: { id } });
+    },
+    addToShopCar() {
+      this.ballFlag = !this.ballFlag;
+    },
+    beforeEnter(el) {
+      el.style.transform = "translate(0,0)";
+    },
+    enter(el, done) {
+      el.offsetWidth;
+      //获取小球和购物车数量标位置
+      const ballPosition = this.$refs.ball.getBoundingClientRect();
+      const badgePosition = document
+        .getElementById("badge")
+        .getBoundingClientRect();
+      const xDist = badgePosition.left - ballPosition.left;
+      const yDist = badgePosition.top - ballPosition.top;
+
+      el.style.transform = `translate(${xDist}px,${yDist}px)`;
+      el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+      done();
+    },
+    afterEnter(el) {
+      this.ballFlag = !this.ballFlag;
     }
   },
   components: {
@@ -97,6 +124,7 @@ export default {
 
 <style lang="scss" scoped>
 .goodsinfo-container {
+  position: relative;
   background-color: #eee;
   overflow: hidden;
   .mint-swipe {
@@ -130,6 +158,17 @@ export default {
 
   .mui-card-footer {
     display: block;
+  }
+
+  .ball {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: red;
+    position: absolute;
+    z-index: 1;
+    left: 150px;
+    top: 350px;
   }
 }
 </style>
